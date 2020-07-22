4df063055f8a259529e4f4ad4f341c0d9b7ae01e
Coța Octavian <octavian_cota@yahoo.com>
2020-07-22 15:12:03 +0300

    Update solve.c

--- #4/solve.c
+++ #4/solve.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-#define TM_PRINTF(debugLevel, f_, ...) printf("%d %s %d\n", debugLevel, __FILE__, __LINE__); printf((f_), __VA_ARGS__)
+/*    
+    * @brief This macro prints out information about the file, the line and the formatted text
+    * @param[in] - debug_level is an integer from 1 to 5
+    *            - f_ is the format of the text
+    *            - ... stores one or more parameters of everything you want to print out
+*/
 
+#define TM_PRINTF(debug_level, f_, ...) \
+    printf("%d %s %d\n", debug_level, __FILE__, __LINE__); \
+    printf((f_), __VA_ARGS__)
+    
+    
 int main() 
 {
     TM_PRINTF(5, "My value is %d\n", 5);
