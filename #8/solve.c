98878b3d5e8038c16cd38a34e151c3d74197fbb5
Coța Octavian <octavian_cota@yahoo.com>
2020-07-22 11:02:39 +0300

    Update solve.c

--- #8/solve.c
+++ #8/solve.c
@@ -4,18 +4,22 @@
 
 #define THREAD_SIZE 10
 
-void *printInfo(void *args) {
+void *printInfo(void *args) 
+{
     int *ptr = (int *) args;
     printf("%d\n", *ptr);
 
     return NULL;
 }
 
-int main() {
+int main() 
+{
     pthread_t thread_id[THREAD_SIZE];
     int i;
     int v = 0;
-    for(i = 0; i < THREAD_SIZE; i++) {
+    
+    for (i = 0; i < THREAD_SIZE; i++) 
+    {
         pthread_create(&thread_id[i], NULL, printInfo, &i);
         pthread_join(thread_id[i], NULL);
     }
