3c4cd1f002fe90093e22f1ad8b54bf57c106426c
Coța Octavian <octavian_cota@yahoo.com>
2020-07-22 16:00:03 +0300

    Update solve.c

--- #10/solve.c
+++ #10/solve.c
@@ -1,60 +1,54 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define THREAD_SIZE 10
 
-pthread_mutex_t lock;
+static bool run = true;
 
-void *printInfo(void *args) 
+/*
+    * @brief - This function prints out the value passed as a parameter
+    * @param[in] - void *args is the variable that is going to be printed passed as a parameter to thread's function
+    * @return - returns NULL 
+*/
+void *processing(void *args) 
 {
-    /* Lock the thread using mutex */
-    pthread_mutex_lock(&lock);
 
-    /* Get the variable from args */
-    int *ptr = (int *) args;    
+    int *noOfThread = (int *) args;
 
-    printf("Job %d started.\n", *ptr);
+    printf("%s%d%s\n", "Thread #", *noOfThread, " is working.");
 
-    printf("Job %d finished.", *ptr);
+    while (run) 
+    {
 
-    printf("\n------------------------------\n");
+    }
 
-    /* Increment it */
-    (*ptr)++;
-
-    /* Unlock the thread using mutex */
-    pthread_mutex_unlock(&lock);
+    printf("%s%d%s\n", "Thread #", *noOfThread, " stopped.");
 
     return NULL;
 }
 
-int main() 
-{
+int main() {
     pthread_t tid[THREAD_SIZE];
-
-    if (pthread_mutex_init(&lock, NULL) != 0) 
-    {
-        printf("\n Something went wrong while initializing the mutex.\n");
-        return 1;
-    }
-
-    printf("------------------------------\n");
+    int v[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int i;
-    int v = 0;
-    
     for (i = 0; i < THREAD_SIZE; i++) 
     {
-        pthread_create(&tid[i], NULL, printInfo, &v);
+        pthread_create(&tid[i], NULL, processing, &v[i]); 
     }
 
+    sleep(5);
+
+    run = false;
+
     for (i = 0; i < THREAD_SIZE; i++) 
     {
-        pthread_join(tid[i], NULL);
-    }   
+        pthread_join(tid[i], NULL); 
+    }
 
-    pthread_mutex_destroy(&lock);
-    
     return 0;
 }
