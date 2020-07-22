ad608be54da266a787c9c9a563b42bb06f1db364
Coța Octavian <octavian_cota@yahoo.com>
2020-07-22 10:59:52 +0300

    Update solve.c

--- #4/solve.c
+++ #4/solve.c
@@ -2,7 +2,8 @@
 
 #define TM_PRINTF(debugLevel, f_, ...) printf("%d %s %d\n", debugLevel, __FILE__, __LINE__); printf((f_), __VA_ARGS__)
 
-int main() {
+int main() 
+{
     TM_PRINTF(5, "My value is %d\n", 5);
     return 0;
 }
