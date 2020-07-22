a4ab62d790b46cf3ced544d9bbabd90a91d60703
Coța Octavian <octavian_cota@yahoo.com>
2020-07-22 15:06:58 +0300

    Update solve.c

--- #2/solve.c
+++ #2/solve.c
@@ -3,19 +3,28 @@
 #include <stdint.h>
 #include <string.h>
 
-uint32_t getValue(char *buffer, int offset, int bufferSize) 
+/* 
+ * @brief Function used to get the first number from a string
+ * @param[in] - char *buffer is the string that is going to be parsed
+ *            - int offset is the index from where you start searching for a number
+ *            - int buffer_size is the parameter that specifies the size of the string
+ * @return - returns the a number if exists in string, else is going to return an error based on a specific case
+*/
+
+uint32_t get_value(char *buffer, int offset, int buffer_size) 
 {
 
-    // Check if the buffer is not empty.
-    if (bufferSize == 0) 
+    /* Check if the buffer is not empty. */
+    if (buffer_size == 0) 
     {
         return -1;
     } 
-    else if (offset > bufferSize) { // Check if the offset is not greater than bufferSize.
+    /* Check if the offset is not greater than buffer_size. */
+    else if (offset > buffer_size) { 
         return -2;
     } 
     else 
-    { // Search for uint32_t value in the buffer.
+    {   /* Search for uint32_t value in the buffer. */
         int i;
         for (i = offset; buffer[i]; i++) 
         {
@@ -34,18 +43,18 @@ uint32_t getValue(char *buffer, int offset, int bufferSize)
 int main() {
     char *buffer = "Tes3tare4";
 
-    // Check if the buffer is not NULL;
+    /* Check if the buffer is not NULL; */
     if (buffer != NULL) 
     {
-        // Get the function's result.
-        uint32_t result = getValue(buffer, 5, strlen(buffer) - 1);
+        /* Get the function's result. */
+        uint32_t result = get_value(buffer, 5, strlen(buffer) - 1);
         switch (result) 
         {
             case -1:
                 printf("%s\n", "The buffer is empty.");
                 break;
             case -2:
-                printf("%s\n", "The offset is greater than bufferSize.");
+                printf("%s\n", "The offset is greater than buffer_size.");
                 break;
             case -3: 
                 printf("%s\n", "The buffer doesn't contain any numbers.");
