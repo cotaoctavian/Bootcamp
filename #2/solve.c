0567ade3dba8642a38c9980b693b152a1d46d28c
Coța Octavian <octavian_cota@yahoo.com>
2020-07-28 10:54:17 +0300

    Update solve.c

--- #2/solve.c
+++ #2/solve.c
@@ -3,71 +3,57 @@
 #include <stdint.h>
 #include <string.h>
 
-/* 
- * @brief Function used to get the first number from a string
- * @param[in] - char *buffer is the string that is going to be parsed
- *            - int offset is the index from where you start searching for a number
- *            - int buffer_size is the parameter that specifies the size of the string
- * @return - returns the a number if exists in string, else is going to return an error based on a specific case
+/** 
+ * @brief         - Function used to get the first number from a string
+ * @param[in]     - *buffer - is the string that is going to be parsed
+ *                - offset - is the index from where you start searching for a number
+ *                - buffer_size - is the parameter that specifies the size of the string
+ * @param[in/out] - result - is the converted integer from the buffer's bytes.
+ * @return        - returns the a number if exists in string, else is going to return an error based on a specific case
 */
+uint32_t get_value(char *buffer, int offset, int buffer_size, uint32_t *result) 
+{   
+    
+    int red_flag = 0;
 
-uint32_t get_value(char *buffer, int offset, int buffer_size) 
-{
-
-    /* Check if the buffer is not empty. */
-    if (buffer_size == 0) 
+    if (buffer == NULL) 
     {
-        return -1;
-    } 
-    /* Check if the offset is not greater than buffer_size. */
-    else if (offset > buffer_size) { 
-        return -2;
+        printf("%s\n", "The buffer is null.");
+        red_flag = -1;
     } 
     else 
-    {   /* Search for uint32_t value in the buffer. */
-        int i;
-        for (i = offset; buffer[i]; i++) 
-        {
-            if (isdigit(buffer[i]))
-            {
-                return (uint32_t) buffer[i] - 48;
-            }
-        }
-
-        return -3;
-    }
-
-    return 0;
-}
-
-int main() {
-    char *buffer = "Tes3tare4";
-
-    /* Check if the buffer is not NULL; */
-    if (buffer != NULL) 
     {
-        /* Get the function's result. */
-        uint32_t result = get_value(buffer, 5, strlen(buffer) - 1);
-        switch (result) 
+        if (offset > buffer_size - 4) 
         {
-            case -1:
-                printf("%s\n", "The buffer is empty.");
-                break;
-            case -2:
-                printf("%s\n", "The offset is greater than buffer_size.");
-                break;
-            case -3: 
-                printf("%s\n", "The buffer doesn't contain any numbers.");
-                break;
-            default:
-                printf("%d\n", result);
-                break;
+            printf("%s\n", "Offset is greater than maximum limit.");
+            red_flag = -2;
+        } 
+        else 
+        { 
+            *result = *(uint32_t *) (buffer + offset);
         }
     }
-    else
-    {
-        printf("%s\n", "The buffer is NULL.");
-    }
-    
-    return 0;
+
+    return red_flag;
 }
+
+
+int main() 
+{ 
+
+    char buffer[] = {0x01, 0x11, 0x11, 0x00, 0x00, 0x00, 0x01}; 
+    uint32_t result = 0;
+
+    int response = get_value(buffer, 2, 7, &result);
+
+    if (response == 0) 
+    {
+        printf("%d\n", result);
+    } 
+    else 
+    {
+        printf("%s\n", "Something went wrong while trying to convert the buffer to integer.");
+    }
+
+    return 0; 
+} 
