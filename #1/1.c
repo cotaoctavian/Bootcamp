c2874f8f9aaf5c24dea25eb09c792b8779016989
Coța Octavian <octavian_cota@yahoo.com>
2020-07-28 09:12:35 +0300

    Update 1.c

--- #1/1.c
+++ #1/1.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/* 
-    * @brief Converting from base 10 to base 2.
-    * @param[in] uint32_t value - integer in base 10
+/**
+* @brief     - Converting from base 10 to base 2.
+* @param[in] - value - is integer in base 10
 */
 
 void print_bits(uint32_t value)
 {
-    for (unsigned bit = 1u << 31; bit != 0; bit >>= 1) 
+
+    unsigned int bit = 1u << 31;
+
+    while (bit) 
     {
         printf("%c", (value & bit) ? '1' : '0');
+        bit >>= 1;
     }
 
     printf("\n");
