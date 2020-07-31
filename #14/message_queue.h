5e2f014ce747c645d118daf93231d1d7a2b7756c
Coța Octavian <octavian_cota@yahoo.com>
2020-07-31 13:12:56 +0300

    Update message_queue.h

--- #14/message_queue.h
+++ #14/message_queue.h
@@ -16,13 +16,29 @@ typedef struct _MessageQueue
     pthread_mutex_t lock;
 } MessageQueue;
 
-/**************************************************************
- *                PUBLIC FUNCTIONS DECLARATION                *
- **************************************************************/
-
+/** 
+* @brief  Initialize a new queue. 
+* @return returns the initialized queue
+*/
 MessageQueue *initialize_message_queue(void);
+
+/**
+ * @brief     Deinitialize the queue.
+ * @param[in] queue - is the queue that is going to be deinitialized
+ */
 void deinitialize_message_queue(MessageQueue *queue);
+
+/**
+* @brief     Function that adds an element to the queue
+* @param[in] queue - is the queue that is going to be modified.
+* @param[in] word  - is the string that is going to be added into the queue.
+*/
 void send_message(MessageQueue *queue, char word[]);
-void receive_message(MessageQueue *queue);
+
+/** 
+* @brief  Function that removes the top element from the queue
+* @return returns the string popped from the queue
+*/
+char *receive_message(MessageQueue *queue);
 
 #endif
